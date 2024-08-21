5725cf8425928af461dbc528b9d06ca38cf205d1
KimYonai <msb0714@gmail.com>
2024-08-21 17:56:05 +0900

    fix : Controller 스크립트 코드 수정
    Controller 스크립트의 이동 키를 화살표에서 WASD로 변경했습니다.

--- Assets/KimYunHwan/Controller.cs
+++ Assets/KimYunHwan/Controller.cs
@@ -14,19 +14,19 @@ public class Controller : MonoBehaviour
 
     public void Update()
     {
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.A))
         {
             rigid.AddForce(Vector3.left * movePower, ForceMode.Force);
         }
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.D))
         {
             rigid.AddForce(Vector3.right * movePower, ForceMode.Force);
         }
-        if (Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKey(KeyCode.W))
         {
             rigid.AddForce(Vector3.forward * movePower, ForceMode.Force);
         }
-        if (Input.GetKey(KeyCode.DownArrow))
+        if (Input.GetKey(KeyCode.S))
         {
             rigid.AddForce(Vector3.back * movePower, ForceMode.Force);
         }
